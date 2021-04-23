6b386ef6cdaa8a5bab40d2b097e71f9a5e54bade
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-04-23 19:51:55 +0530

    added inline mode in reddit

--- caligo/modules/reddit.py
+++ caligo/modules/reddit.py
@@ -1,8 +1,17 @@
+import logging
 import re
-from typing import ClassVar, Dict, Optional, Pattern
+from typing import ClassVar, Dict, List, Optional, Pattern
 
 import aiohttp
-from pyrogram.types import InlineQuery
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InlineQuery,
+    InlineQueryResultAnimation,
+    InlineQueryResultArticle,
+    InlineQueryResultPhoto,
+    InputTextMessageContent,
+)
 
 from .. import command, listener, module, util
 
@@ -12,6 +21,7 @@ class RedditDl(module.Module):
     http: aiohttp.ClientSession
     uri: str
     thumb_regex: Pattern
+    max_inline_results: int
 
     async def on_load(self) -> None:
         self.thumb_regex = re.compile(
@@ -19,6 +29,7 @@ class RedditDl(module.Module):
         )
         self.http = self.bot.http
         self.uri = "https://meme-api.herokuapp.com/gimme"
+        self.max_inline_results = "30"
 
     def get_rthumb(self, result: Dict) -> str:
         """get thumbnail of size 210 and above"""
@@ -35,10 +46,8 @@ class RedditDl(module.Module):
 
     @staticmethod
     def parse_rpost(r: Dict) -> Optional[Dict[str, str]]:
-        if r.get("code"):
-            return f"**ERROR (code: {r['code']})** : `{r.get('message')}`"
         if not r.get('url'):
-            return "Coudn't find any reddit post with image or gif, Please try again"
+            return logging.error()
         caption = f"""
 <b>{r['title']}</b>
 <code>Posted by u/{r['author']}
@@ -52,30 +61,33 @@ class RedditDl(module.Module):
                     subreddit=r["subreddit"],
                     media_url=r["url"])
 
-    # @command.desc("get post from reddit")
+    @command.desc("get post from reddit")
     async def cmd_reddit(self, ctx: command.Context):
         await ctx.respond("`Processing ...`")
+        r_api = "/".join([self.uri, ctx.input.split()[0]]) if ctx.input else self.uri
         rjson = await util.aiorequest(session=self.http,
-                                      url=self.uri,
+                                      url=r_api,
                                       mode="json")
-        result = self.parse_rpost(rjson)
-        if isinstance(result, str):  # Error
-            return result
+        if rjson is None:
+            return "ERROR : Reddit API is Down !"
+        if rjson.get("code"):
+            return f"**ERROR (code: {res['code']})** : `{res.get('message')}`"
+        if (res := self.parse_rpost(rjson)) is None:
+            return "Coudn't find any reddit post with image or gif, Please try again"
         chat_id = ctx.msg.chat.id
         reply_id = ctx.msg.reply_to_message.message_id if ctx.msg.reply_to_message else None
-        caption = result[
-            "caption"] + f"\nSource: [r/{result['subreddit']}]({result['postlink']})"
-        if result["media_url"].endswith(".gif"):
+        caption = f"{res['caption']}\nSource: [r/{res['subreddit']}]({res['postlink']})"
+        if res["media_url"].endswith(".gif"):
             await self.bot.client.send_animation(
                 chat_id=chat_id,
-                animation=result["media_url"],
+                animation=res["media_url"],
                 caption=caption,
                 reply_to_message_id=reply_id,
             )
         else:
             await self.bot.client.send_photo(
                 chat_id=chat_id,
-                photo=result["media_url"],
+                photo=res["media_url"],
                 caption=caption,
                 reply_to_message_id=reply_id,
             )
@@ -84,6 +96,58 @@ class RedditDl(module.Module):
     @listener.pattern(r"(?i)^reddit\s{0,}(?:(?:r/)?([A-Za-z]+)\.)?$")
     async def on_inline_query(self, query: InlineQuery) -> None:
         if subreddit := query.matches[0].group(1):
-            pass
+            r_api = "/".join([self.uri, subreddit, self.max_inline_results])
         else:
-            pass
+            r_api = "/".join([self.uri, self.max_inline_results])
+        rjson = await util.aiorequest(session=self.http, url=r_api, mode="json")
+        if rjson is None:
+            result = "Coudn't find any reddit post with image or gif, Please try again"
+        elif rjson.get("code"):
+            result = f"**ERROR (code: {rjson['code']})** : `{rjson.get('message')}`"
+        else:
+            results: List = []
+            for post in rjson.get("memes"):
+                if p_data := self.parse_rpost(post):
+                    thumbnail = self.get_rthumb(p_data)
+                    buttons = InlineKeyboardMarkup([[
+                        InlineKeyboardButton(f"Source: r/{p_data['subreddit']}",
+                                             url=p_data['postlink'])
+                    ]])
+                    if p_data['media_url'].endswith(".gif"):
+                        results.append(
+                            InlineQueryResultAnimation(
+                                animation_url=p_data['media_url'],
+                                thumb_url=thumbnail,
+                                caption=p_data['caption'],
+                                reply_markup=buttons,
+                            ))
+                    else:
+                        results.append(
+                            InlineQueryResultPhoto(
+                                photo_url=p_data['media_url'],
+                                thumb_url=thumbnail,
+                                caption=p_data['caption'],
+                                reply_markup=buttons,
+                            ))
+
+        if not results:
+            results = "Coudn't find any reddit post with image or gif, Please try again"
+
+        if isinstance(results, str):
+            switch_pm_text = "⚠️ Error"
+            results = [
+                InlineQueryResultArticle(
+                    title=results,
+                    input_message_content=InputTextMessageContent(results),
+                    thumb_url="https://i.imgur.com/7a7aPVa.png",
+                )
+            ]
+        else:
+            switch_pm_text = "Usage: [reddit|reddit subreddit.]"
+
+        await query.answer(results=results,
+                           cache_time=5,
+                           is_gallery=len(results) > 1,
+                            switch_pm_text=switch_pm_text,
+                            switch_pm_parameter="inline"
+        )
